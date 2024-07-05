659c02aeb03e701284439da8e7bb20c6e0b7f4ba
Haluan157 <wordcarikata337@gmail.com>
2024-07-05 16:59:01 +0700

    Create cinta.js

---
+++ matematika/vl/cinta.js
@@ -0,0 +1,32 @@
+$("#panjang").add("#segsik").hide()
+
+var f,kel,lu,ff,fff;
+
+$("#fat").on('change', function() {
+  let a = $("#fat").val();
+  if (a=="a"){
+    $("#persegi").show()
+    $("#panjang").add(".jawaban").add("#segsik").hide()
+    $("#halo").val("")
+  }else {
+    $("#panjang").show()
+    $("#persegi").add(".jawaban").add("#segsik").hide()
+    $("#hal").add("#hola").add("#hol").val("")
+  }
+})
+//Persegi
+$("#hper").click(function(){
+      f = Number($("#halo").val())
+      kel = f*f*4
+      lu = f**3
+      $(".jawaban").show().text("Volume = "+ lu+", Luas = "+ kel)
+    })
+//Persegi Panjang
+$("#hpan").click(function(){
+      f = Number($("#hal").val())
+      ff = Number($("#hola").val())
+      fff = Number($("#hol").val())
+      kel = (f*f+ff*ff+fff*fff)*2
+      lu = f*ff*fff
+      $(".jawaban").show().text("Volume = "+ lu+", Luas = "+ kel)
+    })
